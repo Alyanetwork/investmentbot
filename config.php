78cff91faf513ddb2a889d147ecfdbfce6af5c71
FYS TEKNOLOJİ <83513785+Alyanetwork@users.noreply.github.com>
2024-11-14 07:27:39 +0300

    Create config.php

---
+++ config.php
@@ -0,0 +1,10 @@
+<?php
+return [
+    // Önceden tanımlanmış ayarlar
+    'telegram_token' => 'YOUR_TELEGRAM_BOT_TOKEN',
+    'twitter_api_key' => 'YOUR_TWITTER_API_KEY',
+    'twitter_api_secret_key' => 'YOUR_TWITTER_API_SECRET',
+    'facebook_access_token' => 'YOUR_FACEBOOK_ACCESS_TOKEN',
+    'discord_webhook_url' => 'YOUR_DISCORD_WEBHOOK_URL'
+];
+?>
