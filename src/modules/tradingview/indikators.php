8d58f1c698abd2d954309b49a83a98b73900c95c
FYS TEKNOLOJİ <83513785+Alyanetwork@users.noreply.github.com>
2024-11-14 07:37:38 +0300

    Create indikators.php

---
+++ src/modules/tradingview/indikators.php
@@ -0,0 +1,30 @@
+public function getChartWithIndicators($symbol, $indicators = [], $width = 800, $height = 600)
+{
+    $indicatorsJs = json_encode($indicators);
+    $html = <<<HTML
+        <div class="tradingview-widget-container">
+            <div id="tradingview_$symbol"></div>
+            <script type="text/javascript" src="https://s3.tradingview.com/tv.js"></script>
+            <script type="text/javascript">
+                new TradingView.widget({
+                    "width": $width,
+                    "height": $height,
+                    "symbol": "$symbol",
+                    "interval": "D",
+                    "timezone": "Etc/UTC",
+                    "theme": "light",
+                    "style": "1",
+                    "locale": "en",
+                    "toolbar_bg": "#f1f3f6",
+                    "enable_publishing": false,
+                    "allow_symbol_change": true,
+                    "studies": $indicatorsJs,
+                    "container_id": "tradingview_$symbol"
+                });
+            </script>
+        </div>
+    HTML;
+
+    return $html;
+}
+
