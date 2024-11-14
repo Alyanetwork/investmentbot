0e7d412cc4d7e855b8b99e2a6186e2314e5054a3
FYS TEKNOLOJİ <83513785+Alyanetwork@users.noreply.github.com>
2024-11-14 07:03:55 +0300

    Create config.php

---
+++ src/config/config.php
@@ -0,0 +1,7 @@
+<?php
+return [
+    'api_key' => 'YOUR_API_KEY_HERE',        // Binance API anahtarı veya diğer veri sağlayıcı API anahtarları
+    'api_secret' => 'YOUR_API_SECRET_HERE',  // Binance API gizli anahtarı
+    'api_base_url' => 'https://api.binance.com' // Binance API URL’si
+];
+?>
