cd1e2a179f745200d9846c45d21d41d18de7ec38
FYS TEKNOLOJİ <83513785+Alyanetwork@users.noreply.github.com>
2024-11-14 08:05:58 +0300

    Create JWT.php

---
+++ src/utils/JWT.php
@@ -0,0 +1,38 @@
+<?php
+
+class JWT
+{
+    private static $secretKey = "YOUR_SECRET_KEY"; // Güvenli bir secret key girin
+
+    public static function generateToken($payload)
+    {
+        $header = json_encode(['typ' => 'JWT', 'alg' => 'HS256']);
+        $payload = json_encode($payload);
+        $base64UrlHeader = str_replace(['+', '/', '='], ['-', '_', ''], base64_encode($header));
+        $base64UrlPayload = str_replace(['+', '/', '='], ['-', '_', ''], base64_encode($payload));
+        $signature = hash_hmac('sha256', $base64UrlHeader . "." . $base64UrlPayload, self::$secretKey, true);
+        $base64UrlSignature = str_replace(['+', '/', '='], ['-', '_', ''], base64_encode($signature));
+
+        return $base64UrlHeader . "." . $base64UrlPayload . "." . $base64UrlSignature;
+    }
+
+    public static function validateToken($token)
+    {
+        $parts = explode(".", $token);
+        if (count($parts) != 3) {
+            return false;
+        }
+
+        $header = $parts[0];
+        $payload = $parts[1];
+        $signatureProvided = $parts[2];
+
+        $base64UrlHeader = str_replace(['+', '/', '='], ['-', '_', ''], base64_encode($header));
+        $base64UrlPayload = str_replace(['+', '/', '='], ['-', '_', ''], base64_encode($payload));
+        $signature = hash_hmac('sha256', $base64UrlHeader . "." . $base64UrlPayload, self::$secretKey, true);
+        $base64UrlSignature = str_replace(['+', '/', '='], ['-', '_', ''], base64_encode($signature));
+
+        return ($base64UrlSignature === $signatureProvided);
+    }
+}
+?>
